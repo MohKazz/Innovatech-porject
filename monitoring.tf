27601a12b58624e3daf1eec95f0e8c6e3858810c
Mohammed-khazzan <540149@student.fontys.nl>
2026-01-13 15:37:20 +0100

    updating the instance type,  fixing the config file

--- monitoring.tf
+++ monitoring.tf
@@ -16,22 +16,26 @@ resource "aws_security_group_rule" "monitor_egress_all" {
   security_group_id = aws_security_group.monitoring.id
 }
 
-
 # IAM role/profile for monitoring instance
 resource "aws_iam_role" "monitor" {
   name = "${var.name}-monitor-role"
   assume_role_policy = jsonencode({
     Version   = "2012-10-17",
-    Statement = [{ Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" }, Action = "sts:AssumeRole" }]
+    Statement = [{ 
+        Effect = "Allow", 
+        Principal = {
+             Service = "ec2.amazonaws.com" }, 
+        Action = "sts:AssumeRole" 
+        }]
   })
   tags = var.tags
 }
-
+# this policy allows the monitoring instance to use SSM 
 resource "aws_iam_role_policy_attachment" "monitor_ssm" {
   role       = aws_iam_role.monitor.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
-
+# this policy allows the monitoring instance to read EC2 metadata
 resource "aws_iam_role_policy_attachment" "monitor_ec2_read" {
   role       = aws_iam_role.monitor.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
@@ -46,8 +50,8 @@ resource "aws_iam_instance_profile" "monitor" {
 # Monitoring instance
 resource "aws_instance" "monitor" {
   ami                         = data.aws_ami.amazon_linux2.id
-  instance_type               = "t3.micro"
-  subnet_id                 = aws_subnet.public["a"].id
+  instance_type               = "t2.micro" #changed from t3.micro to t2.micro for cost savings
+  subnet_id                   = aws_subnet.public_a.id
   vpc_security_group_ids      = [aws_security_group.monitoring.id]
   iam_instance_profile        = aws_iam_instance_profile.monitor.name
   associate_public_ip_address = false
