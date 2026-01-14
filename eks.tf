14c225f3a0a8b32327fb2ce2829f99a07b031a9c
Mohammed-khazzan <540149@student.fontys.nl>
2026-01-14 14:00:48 +0100

    pushing updates for eks + the merged networking files (vpc, subnets,nsecurity groups, routes, route tables, providers, gateways...etc) into one main.tf file

--- eks.tf
+++ eks.tf
@@ -124,3 +124,17 @@ resource "aws_eks_node_group" "this" {
   ]
 }
 
+# Retrieve the API endpoint and certificate for the EKS cluster to configure the Kubernetes provider
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
+}
+# Retrieve authentication token for the EKS cluster
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+# Configure the Kubernetes provider so Terraform can create resources inside the EKS cluster
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
